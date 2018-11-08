98740b882ac41781e8ddd659bb3717205a8c8eab
MrLotU <j.koopman@jarict.nl>
2018-11-08 21:56:39 +0100

    Forgot meself a comma :facepalm:

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -3,7 +3,7 @@ import XCTest
 @testable import NIOKitTests
 
 var tests = [
-    testCase(NIOKitTests.allTests)
+    testCase(NIOKitTests.allTests),
     testCase(FutureOperatorTests.allTests)
 ]
 XCTMain(tests)
