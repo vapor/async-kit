2865b645ad190b82cd38a5372f8f6a707df21a4e
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-02-15 12:53:18 -0600

    Created EventLoopFuture<Optional>.mapOptional method

---
+++ Sources/NIOKit/Future+Optional.swift
@@ -0,0 +1,24 @@
+import NIO
+
+extension EventLoopFuture {
+    
+    /// Calls a closure on an optional value in an `EventLoopFuture` if it exists.
+    ///
+    ///     let optional = eventLoop.future(Optiona<Int>.some(42))
+    ///     let some = optional.mapOptional { int -> Float in
+    ///         return int * 3.14
+    ///     }
+    ///     // some: EventLoopFuture(Optional(131.88))
+    ///
+    /// - parameters:
+    ///   - closure: The closure to call on the unwrapped optional value.
+    ///   - unwrapped: The optional's value, unwrapped.
+    /// - returns: The result of the closure if the optional was unwrapped, or nil if it wasn't, wrapped in an `EventLoopFuture`.
+    public func mapOptional<Wrapped, Result>(_ closure: @escaping (_ unwrapped: Wrapped)throws -> Result) -> EventLoopFuture<Result?>
+        where Value == Optional<Wrapped>
+    {
+        return self.flatMapThrowing { optional in
+            return try optional.map(closure)
+        }
+    }
+}
