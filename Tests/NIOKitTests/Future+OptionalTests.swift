32fa08d869474ace05b2b6108939cb272c54e12b
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-02-16 13:17:33 -0600

    Created FutureOptionalTests.testCompactMap and .testCompactFlatMap test cases

--- Tests/NIOKitTests/Future+OptionalTests.swift
+++ Tests/NIOKitTests/Future+OptionalTests.swift
@@ -3,8 +3,8 @@ import XCTest
 
 final class FutureOptionalTests: NIOKitTestCase {
     func testMapOptional() throws {
-        let future = self.group.next().makeSucceededFuture(Optional<Int>.some(1))
-        let null = self.group.next().makeSucceededFuture(Optional<Int>.none)
+        let future = self.eventLoop.makeSucceededFuture(Optional<Int>.some(1))
+        let null = self.eventLoop.makeSucceededFuture(Optional<Int>.none)
         
         let times2 = future.mapOptional { $0 * 2 }
         let null2 = null.mapOptional { $0 * 2 }
@@ -13,12 +13,32 @@ final class FutureOptionalTests: NIOKitTestCase {
         try XCTAssertEqual(nil, null2.wait())
     }
     
+    func testCompactMap() throws {
+        let future = self.eventLoop.makeSucceededFuture(Optional<Int>.some(1))
+        
+        let times2 = future.compactMap { $0 * 2 }
+        let null2 = future.compactMap { return $0 % 2 == 0 ? $0 : nil }
+        
+        try XCTAssertEqual(2, times2.wait())
+        try XCTAssertEqual(nil, null2.wait())
+    }
+    
     func testFlatMapOptional() throws {
-        let future = self.group.next().makeSucceededFuture(Optional<Int>.some(1))
-        let null = self.group.next().makeSucceededFuture(Optional<Int>.none)
+        let future = self.eventLoop.makeSucceededFuture(Optional<Int>.some(1))
+        let null = self.eventLoop.makeSucceededFuture(Optional<Int>.none)
         
         let times2 = future.flatMapOptional { self.multiply($0, 2) }
         let null2 = null.flatMapOptional { self.multiply($0, 2) }
+        
+        try XCTAssertEqual(2, times2.wait())
+        try XCTAssertEqual(nil, null2.wait())
+    }
+    
+    func testCompactFlatMap() throws {
+        let future = self.eventLoop.makeSucceededFuture(Optional<Int>.some(1))
+        
+        let times2 = future.compactFlatMap { self.multiply($0, Optional<Int>.some(2)) }
+        let null2 = future.compactFlatMap { self.multiply($0, nil) }
         
         try XCTAssertEqual(2, times2.wait())
         try XCTAssertEqual(nil, null2.wait())
@@ -28,8 +48,14 @@ final class FutureOptionalTests: NIOKitTestCase {
         return self.group.next().makeSucceededFuture(a * b)
     }
     
+    func multiply(_ a: Int, _ b: Int?) -> EventLoopFuture<Int?> {
+        return self.group.next().makeSucceededFuture(b == nil ? nil : a * b!)
+    }
+    
     static let allTests = [
         ("testMapOptional", testMapOptional),
-        ("testFlatMapOptional", testFlatMapOptional)
+        ("testCompactMap", testCompactMap),
+        ("testFlatMapOptional", testFlatMapOptional),
+        ("testCompactFlatMap", testCompactFlatMap)
     ]
 }
