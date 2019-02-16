deb6fe87d122676a609947bb6c46d940bbadef27
MrLotU <j.koopman@jarict.nl>
2019-02-16 18:20:49 +0100

    Docs and remove protocol
    Signed-off-by: MrLotU <j.koopman@jarict.nl>

--- Sources/NIOKit/NIOKitTestCase.swift
+++ Sources/NIOKit/NIOKitTestCase.swift
@@ -1,35 +1,36 @@
 import NIO
 import XCTest
 
-protocol NIOKitTestCaseProtocol: class {
-    var group: EventLoopGroup! { get set }
-}
-
-extension NIOKitTestCaseProtocol {
-    public func setupGroup() {
-        self.group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
-    }
-    
-    public func teardownGroup() {
-        XCTAssertNoThrow(try self.group.syncShutdownGracefully())
-        self.group = nil
-    }
-}
-
-open class NIOKitTestCase: XCTestCase, NIOKitTestCaseProtocol {
+/// Utilitarian class to create tests that need an EventLoop
+///
+///     class MyTestCase: NIOKitTestCase {
+///         
+///         func testSomeFuture() throws {
+///             XCTAssertEqual(self.eventLoop.makeSucceededFuture(8).wait(), 8)
+///         }
+///     }
+///
+open class NIOKitTestCase: XCTestCase {
+    /// This TestCases EventLoopGroup
     open var group: EventLoopGroup!
     
+    /// Returns the next EventLoop from the `group`
     open var eventLoop: EventLoop {
         return self.group.next()
     }
     
+    /// Sets up the TestCase for use
+    /// and initializes the EventLoopGroup
     override open func setUp() {
-        super.setUp()
+        self.group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
         setupGroup()
     }
     
+    /// Tears down the TestCase and
+    /// shuts down the EventLoopGroup
     override open func tearDown() {
-        teardownGroup()
+        XCTAssertNoThrow(try self.group.syncShutdownGracefully())
+        self.group = nil
         super.tearDown()
     }
 }
