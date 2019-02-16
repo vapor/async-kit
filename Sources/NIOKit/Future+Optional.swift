7e1ae17b193ca3f541353893491087d7ed3897b1
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-02-16 13:03:45 -0600

    Created EventLoopFuture.compactMap(_:) and .compactFlatMap(_:) methods

--- Sources/NIOKit/Future+Optional.swift
+++ Sources/NIOKit/Future+Optional.swift
@@ -22,10 +22,30 @@ extension EventLoopFuture {
         }
     }
     
+    /// Calls a closure that returns an optional on an optional value in an `EventLoopFuture` if it exists.
+    ///
+    ///     let optional = eventLoop.future(Optiona<Int>.some(42))
+    ///     let some = optional.mapOptional { int -> Float? in
+    ///         return int * Optional<Float>.some(3.14)
+    ///     }
+    ///     // some: EventLoopFuture(Optional(131.88))
+    ///
+    /// - parameters:
+    ///   - closure: The closure to call on the unwrapped optional value.
+    ///   - unwrapped: The optional's value, unwrapped.
+    /// - returns: The result of the closure if the optional was unwrapped, or nil if it wasn't, wrapped in an `EventLoopFuture`.
+    public func compactMap<Wrapped, Result>(_ closure: @escaping (_ unwrapped: Wrapped)throws -> Result?) -> EventLoopFuture<Result?>
+        where Value == Optional<Wrapped>
+    {
+        return self.flatMapThrowing { optional in
+            return try optional.flatMap(closure)
+        }
+    }
+    
     /// Calls a closure on an optional value in an `EventLoopFuture` if it exists.
     ///
     ///     let optional = eventLoop.future(Optiona<Int>.some(42))
-    ///     let some = optional.mapOptional { int -> Float in
+    ///     let some = optional.mapOptional { int -> EventLoopFuture<Float> in
     ///         return int * 3.14
     ///     }
     ///     // some: EventLoopFuture(Optional(131.88))
@@ -41,6 +61,31 @@ extension EventLoopFuture {
         return self.flatMap { optional -> EventLoopFuture<Result?> in
             do {
                 return try optional.map(closure)?.map { $0 } ?? self.eventLoop.makeSucceededFuture(Optional<Result>.none)
+            } catch let error {
+                return self.eventLoop.makeFailedFuture(error)
+            }
+        }
+    }
+    
+    /// Calls a closure that returns an optional future on an optional value in an `EventLoopFuture` if it exists.
+    ///
+    ///     let optional = eventLoop.future(Optiona<Int>.some(42))
+    ///     let some = optional.mapOptional { int -> EventLoopFuture<Float?> in
+    ///         return int * Optional<Float>.some(3.14)
+    ///     }
+    ///     // some: EventLoopFuture(Optional(131.88))
+    ///
+    /// - parameters:
+    ///   - closure: The closure to call on the unwrapped optional value.
+    ///   - unwrapped: The optional's value, unwrapped.
+    /// - returns: The result of the closure if the optional was unwrapped, or nil if it wasn't, wrapped in an `EventLoopFuture`.
+    public func compactFlatMap<Wrapped, Result>(
+        _ closure: @escaping (_ unwrapped: Wrapped)throws -> EventLoopFuture<Result?>
+        ) -> EventLoopFuture<Result?> where Value == Optional<Wrapped>
+    {
+        return self.flatMap { optional -> EventLoopFuture<Result?> in
+            do {
+                return try optional.flatMap(closure)?.map { $0 } ?? self.eventLoop.makeSucceededFuture(Optional<Result>.none)
             } catch let error {
                 return self.eventLoop.makeFailedFuture(error)
             }
