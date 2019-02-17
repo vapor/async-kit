1ff91ad62626722d4179a4dac313b686e5cf5ca1
Natanel Niazoff <nniazoff@icloud.com>
2019-02-16 20:23:43 -0500

    Add @autoclosure, change to whenComplete and fix spacing

--- Sources/NIOKit/FutureExtensions.swift
+++ Sources/NIOKit/FutureExtensions.swift
@@ -1,13 +1,17 @@
 // MARK: - Guard
 
 public extension EventLoopFuture {
-	/// Guards that the future's value satisfies the callback's condition or fails with the given error.
-	func `guard`(_ callback: @escaping ((Value) -> Bool), else error: Error) -> EventLoopFuture<Value> {
-		let promise = eventLoop.makePromise(of: Value.self)
-		whenSuccess {
-			if callback($0) { promise.succeed($0) } else { promise.fail(error) }
-		}
-		whenFailure { promise.fail($0) }
-		return promise.futureResult
-	}
+    /// Guards that the future's value satisfies the callback's condition or fails with the given error.
+    func `guard`(_ callback: @escaping ((Value) -> Bool), else error: @escaping @autoclosure () -> Error) -> EventLoopFuture<Value> {
+        let promise = eventLoop.makePromise(of: Value.self)
+        whenComplete { result in
+            switch result {
+            case .success(let value):
+                if callback(value) { promise.succeed(value) }
+                else { promise.fail(error()) }
+            case .failure(let error): promise.fail(error)
+            }
+        }
+        return promise.futureResult
+    }
 }
