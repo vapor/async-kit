daf7d2f57ec00ae9c9cee5a32710e2d05b54c2f8
Natanel Niazoff <nniazoff@icloud.com>
2019-02-17 19:46:45 -0500

    Add more docs

--- Sources/NIOKit/FutureExtensions.swift
+++ Sources/NIOKit/FutureExtensions.swift
@@ -1,7 +1,18 @@
-// MARK: - Guard
-
 public extension EventLoopFuture {
-    /// Guards that the future's value satisfies the callback's condition or fails with the given error.
+    // MARK: - Guard
+    
+    /// Guards that the future's value satisfies the callback's condition or
+    /// fails with the given error.
+    ///
+    ///
+    /// Example usage:
+    ///
+    ///     future.guard({ $0.userID == user.id }, else: AccessError.unauthorized)
+    ///
+    /// - parameters:
+    ///   - callback: Callback that asynchronously executes a condition.
+    ///   - error: The error to fail with if condition isn't met.
+    /// - returns: A future containing the original future's result.
     func `guard`(_ callback: @escaping ((Value) -> Bool), else error: @escaping @autoclosure () -> Error) -> EventLoopFuture<Value> {
         let promise = self.eventLoop.makePromise(of: Value.self)
         self.whenComplete { result in
