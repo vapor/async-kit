250667a3755b6018e8637221d398262f75438c69
Jari (LotU) <j.koopman@jarict.nl>
2019-03-01 19:41:23 +0100

    Add transform (#10)
    * Init
    
    Signed-off-by: MrLotU <j.koopman@jarict.nl>
    
    * Add TestCase to NIOKit instead of test target for inter module/library use
    
    * Make the things public
    
    * Make things open instead of public
    
    * Move some things around, rename
    
    * Configure existing tests to use the new thing
    
    * Add Transform
    
    Signed-off-by: MrLotU <j.koopman@jarict.nl>
    
    * Add other transform function
    
    * Fix remarks
    
    * Reorganize things
    
    Signed-off-by: MrLotU <j.koopman@jarict.nl>
    
    * Fix Swift build issues
    
    Signed-off-by: MrLotU <j.koopman@jarict.nl>

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -4,6 +4,7 @@ import NIOKitTests
 
 var tests = [
     testCase(NIOKitTests.allTests),
+    testCase(TransformTests.allTests),
     testCase(FutureOperatorTests.allTests),
     testCase(ConnectionPoolTests.allTests),
     testCase(FutureExtensionsTests.allTests)
