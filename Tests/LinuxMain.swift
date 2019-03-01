0cf8cdad3eb8119f36f32a534154591b6cc30007
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-01 12:58:37 -0600

    Created FlattenTests test suite

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -4,6 +4,7 @@ import NIOKitTests
 
 var tests = [
     testCase(NIOKitTests.allTests),
+    testCase(FlattenTests.allTests),
     testCase(FutureOperatorTests.allTests),
     testCase(ConnectionPoolTests.allTests),
     testCase(FutureExtensionsTests.allTests)
