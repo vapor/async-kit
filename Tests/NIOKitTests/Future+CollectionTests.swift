56056136a2aff97963f89b602cfc21afa062e109
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-15 17:58:40 -0500

    Created FutureCollectionTests.testFlatMapEach test case

--- Tests/NIOKitTests/Future+CollectionTests.swift
+++ Tests/NIOKitTests/Future+CollectionTests.swift
@@ -2,16 +2,6 @@ import NIOKit
 import XCTest
 
 final class FutureCollectionTests: XCTestCase {
-    private var group: EventLoopGroup!
-    private var eventLoop: EventLoop {
-        return group.next()
-    }
-    
-    override func setUp() {
-        self.group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
-        
-    }
-    
     func testMapEach() throws {
         let collection = eventLoop.makeSucceededFuture([1, 2, 3, 4, 5, 6, 7, 8, 9])
         let times2 = collection.mapEach { int -> Int in int * 2 }
@@ -19,26 +9,35 @@ final class FutureCollectionTests: XCTestCase {
         try XCTAssertEqual(times2.wait(), [2, 4, 6, 8, 10, 12, 14, 16, 18])
     }
     
-    func testThrowMapEach() throws {
+    func testFlatMapEach() throws {
         let collection = eventLoop.makeSucceededFuture([1, 2, 3, 4, 5, 6, 7, 8, 9])
-        let times2 = collection.mapEach { int -> Int in
-            if int == 5 {
-                throw NSError(domain: "com.vapor.niokit", code: 0, userInfo: [:])
-            } else {
-                return int * 2
-            }
+        let times2 = collection.flatMapEach(on: self.eventLoop) { int -> EventLoopFuture<Int> in
+            return self.eventLoop.makeSucceededFuture(int * 2)
         }
         
-        do {
-            _ = try times2.wait()
-            XCTFail("Expected failed future")
-        } catch {
-            // Sucess...
-        }
+        try XCTAssertEqual(times2.wait(), [2, 4, 6, 8, 10, 12, 14, 16, 18])
     }
     
-    static let allTests = [
-        ("testMapEach", testMapEach),
-        ("testThrowMapEach", testThrowMapEach)
-    ]
+    /// This TestCases EventLoopGroup
+    var group: EventLoopGroup!
+    
+    /// Returns the next EventLoop from the `group`
+    var eventLoop: EventLoop {
+        return self.group.next()
+    }
+    
+    /// Sets up the TestCase for use
+    /// and initializes the EventLoopGroup
+    override func setUp() {
+        super.setUp()
+        self.group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+    }
+    
+    /// Tears down the TestCase and
+    /// shuts down the EventLoopGroup
+    override func tearDown() {
+        XCTAssertNoThrow(try self.group.syncShutdownGracefully())
+        self.group = nil
+        super.tearDown()
+    }
 }
