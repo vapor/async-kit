12fc0e0e38d158501ae23bc4b50ee18c5ca25c8f
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-23 11:47:23 -0500

    Eventloop future (#36)
    * Copied EventLoopGroup.future methods from vapor/core repo
    
    * Moved /EventLoopFuture/EventLoop+Future.swift file to /EventLoop directory
    
    * Created EventLoopGroupFutureTests test suite

---
+++ Tests/NIOKitTests/EventLoopGroup+Future.swift
@@ -0,0 +1,49 @@
+import NIOKit
+import XCTest
+
+final class EventLoopGroupFutureTests: XCTestCase {
+    func testFutureVoid() throws {
+        try XCTAssertNoThrow(self.group.future().wait())
+        try XCTAssertNoThrow(self.eventLoop.future().wait())
+    }
+    
+    func testFuture() throws {
+        try XCTAssertEqual(self.group.future(1).wait(), 1)
+        try XCTAssertEqual(self.eventLoop.future(1).wait(), 1)
+        
+        try XCTAssertEqual(self.group.future(true).wait(), true)
+        try XCTAssertEqual(self.eventLoop.future("foo").wait(), "foo")
+    }
+    
+    func testFutureError() throws {
+        let groupErr: EventLoopFuture<Int> = self.group.future(error: TestError.notEqualTo1)
+        let eventLoopErr: EventLoopFuture<String> = self.eventLoop.future(error: TestError.notEqualToBar)
+        
+        try XCTAssertThrowsError(groupErr.wait())
+        try XCTAssertThrowsError(eventLoopErr.wait())
+    }
+    
+    /// This TestCases EventLoopGroup
+    var group: EventLoopGroup!
+    
+    /// Returns the next EventLoop from the `group`
+    var eventLoop: EventLoop {
+        return self.group.next()
+    }
+    
+    /// Sets up the TestCase for use
+    /// and initializes the EventLoopGroup
+    override func setUp() {
+        super.setUp()
+        self.group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+    }
+    
+    /// Tears down the TestCase and
+    /// shuts down the EventLoopGroup
+    override func tearDown() {
+        XCTAssertNoThrow(try self.group.syncShutdownGracefully())
+        self.group = nil
+        super.tearDown()
+    }
+}
+
