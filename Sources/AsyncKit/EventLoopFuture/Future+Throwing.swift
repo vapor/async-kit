2c51621e691308a19ce929155e3d1c32fef815d7
Gwynne Raskind <gwynne@darkrainfall.org>
2020-01-30 16:18:04 -0600

    Add a bit of syntax sugar for starting a future chain with a throwing closure. Somewhat silly test included. (#50)

---
+++ Sources/AsyncKit/EventLoopFuture/Future+Throwing.swift
@@ -0,0 +1,36 @@
+import NIO
+
+extension EventLoopGroup {
+    
+    /// An alternate name for this would be `future(catching:)`, but with that
+    /// name, trailing closure syntax just looks like `el.future { ... }`, which
+    /// does not indicate to readers of the code that it is the error-capturing
+    /// version. Since such an indication is highly desirable, a slightly less
+    /// idiomatic name is used instead.
+    ///
+    /// This method replaces this code:
+    ///
+    /// ```swift
+    /// return something.eventLoop.future().flatMapThrowing {
+    /// ```
+    ///
+    /// With this code:
+    ///
+    /// ```swift
+    /// return something.eventLoop.tryFuture {
+    /// ```
+    ///
+    /// That's pretty much it. It's sugar.
+    ///
+    /// - Parameter work: The potentially throwing closure to execute as a
+    ///   future. If the closure throws, a failed future is returned.
+    public func tryFuture<T>(_ work: () throws -> T) -> EventLoopFuture<T> {
+        do {
+            return self.future(try work())
+        } catch {
+            return self.future(error: error)
+        }
+    }
+
+}
+
