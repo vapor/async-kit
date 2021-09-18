8ec02e274b70afe9e6d2f39c49b7491d76cbc128
Gwynne Raskind <gwynne@darkrainfall.org>
2021-09-18 08:14:14 -0500

    Add EventLoopFuture utility methods for handling empty collection values (#83)
    * Add `EventLoopFuture.nonempty(orError:)`, `.nonemptyMap(or:_:)`, `.nonemptyMap(_:)`, `.nonemptyFlatMapThrowing(or:_:)`, and `.nonemptyFlatMapThrowing(_:)`.
    
    * Add `.nonemptyFlatMap(or:_:)`, `.nonemptyFlatMap(orFlat:_:)`, and `.nonemptyFlatMap(_:)` variants.
    
    * Workaround for Swift 5.2 not permitting the conditional constraint on ELF.Value without a generic parameter on the method itself.

--- Sources/AsyncKit/EventLoopFuture/FutureExtensions.swift
+++ Sources/AsyncKit/EventLoopFuture/FutureExtensions.swift
@@ -1,4 +1,4 @@
-public extension EventLoopFuture {
+extension EventLoopFuture {
     // MARK: - Guard
     
     /// Guards that the future's value satisfies the callback's condition or
@@ -13,7 +13,7 @@ public extension EventLoopFuture {
     ///   - callback: Callback that asynchronously executes a condition.
     ///   - error: The error to fail with if condition isn't met.
     /// - returns: A future containing the original future's result.
-    func `guard`(_ callback: @escaping ((Value) -> Bool), else error: @escaping @autoclosure () -> Error) -> EventLoopFuture<Value> {
+    public func `guard`(_ callback: @escaping ((Value) -> Bool), else error: @escaping @autoclosure () -> Error) -> EventLoopFuture<Value> {
         let promise = self.eventLoop.makePromise(of: Value.self)
         self.whenComplete { result in
             switch result {
@@ -36,7 +36,7 @@ public extension EventLoopFuture {
     ///
     /// This is useful when some work must be done for both success and failure states, especially work that requires
     /// temporarily extending the lifetime of one or more objects.
-    func flatMapAlways<NewValue>(
+    public func flatMapAlways<NewValue>(
         file: StaticString = #file, line: UInt = #line,
         _ callback: @escaping (Result<Value, Error>) -> EventLoopFuture<NewValue>
     ) -> EventLoopFuture<NewValue> {
@@ -44,4 +44,77 @@ public extension EventLoopFuture {
         self.whenComplete { result in callback(result).cascade(to: promise) }
         return promise.futureResult
     }
+    
+    // MARK: - nonempty
+    
+    /// Checks that the future's value (if any) returns `false` for `.isEmpty`. If the check fails, the provided error
+    /// is thrown.
+    public func nonempty<E: Error>(orError error: @escaping @autoclosure () -> E) -> EventLoopFuture<Value> where Value: Collection {
+        return self.guard({ !$0.isEmpty }, else: error())
+    }
+
+    /// Checks that the future's value (if any) returns `false` for `.isEmpty`. If the check fails, a new future with
+    /// the provided alternate value is returned. Otherwise, the provided normal `map()` callback is invoked.
+    public func nonemptyMap<NewValue>(
+        or alternate: @escaping @autoclosure () -> NewValue,
+        _ transform: @escaping (Value) -> NewValue
+    ) -> EventLoopFuture<NewValue> where Value: Collection {
+        return self.map { !$0.isEmpty ? transform($0) : alternate() }
+    }
+
+    /// Checks that the future's value (if any) returns `false` for `.isEmpty`. If the check fails, a new future with
+    /// an empty array as its value is returned. Otherwise, the provided normal `map()` callback is invoked. The
+    /// callback's return type must be an `Array` or a `RangeReplaceableCollection`.
+    public func nonemptyMap<NewValue>(
+        _ transform: @escaping (Value) -> NewValue
+    ) -> EventLoopFuture<NewValue> where Value: Collection, NewValue: RangeReplaceableCollection {
+        return self.nonemptyMap(or: .init(), transform)
+    }
+
+    /// Checks that the future's value (if any) returns `false` for `.isEmpty`. If the check fails, a new future with
+    /// the provided alternate value is returned. Otherwise, the provided normal `flatMapThrowing()` callback is
+    /// invoked.
+    public func nonemptyFlatMapThrowing<NewValue>(
+        or alternate: @escaping @autoclosure () -> NewValue,
+        _ transform: @escaping (Value) throws -> NewValue
+    ) -> EventLoopFuture<NewValue> where Value: Collection {
+        return self.flatMapThrowing { !$0.isEmpty ? try transform($0) : alternate() }
+    }
+
+    /// Checks that the future's value (if any) returns `false` for `.isEmpty`. If the check fails, a new future with
+    /// an empty array as its value is returned. Otherwise, the provided normal `flatMapThrowing()` callback is
+    /// invoked. The callback's return type must be an `Array` or a `RangeReplaceableCollection`.
+    public func nonemptyFlatMapThrowing<NewValue>(
+        _ transform: @escaping (Value) throws -> NewValue
+    ) -> EventLoopFuture<NewValue> where Value: Collection, NewValue: RangeReplaceableCollection {
+        return self.nonemptyFlatMapThrowing(or: .init(), transform)
+    }
+
+    /// Checks that the future's value (if any) returns `false` for `.isEmpty`. If the check fails, a new future with
+    /// the provided alternate value is returned. Otherwise, the provided normal `flatMap()` callback is invoked.
+    public func nonemptyFlatMap<NewValue>(
+        or alternate: @escaping @autoclosure () -> NewValue,
+        _ transform: @escaping (Value) -> EventLoopFuture<NewValue>
+    ) -> EventLoopFuture<NewValue> where Value: Collection {
+        return self.nonemptyFlatMap(orFlat: self.eventLoop.makeSucceededFuture(alternate()), transform)
+    }
+
+    /// Checks that the future's value (if any) returns `false` for `.isEmpty`. If the check fails, the provided
+    /// alternate future is returned. Otherwise, the provided normal `flatMap()` callback is invoked.
+    public func nonemptyFlatMap<NewValue>(
+        orFlat alternate: @escaping @autoclosure () -> EventLoopFuture<NewValue>,
+        _ transform: @escaping (Value) -> EventLoopFuture<NewValue>
+    ) -> EventLoopFuture<NewValue> where Value: Collection {
+        return self.flatMap { !$0.isEmpty ? transform($0) : alternate() }
+    }
+
+    /// Checks that the future's value (if any) returns `false` for `.isEmpty`. If the check fails, a new future with
+    /// an empty array as its value is returned. Otherwise, the provided normal `flatMap()` callback is invoked. The
+    /// callback's returned future must have a value type that is an `Array` or a `RangeReplaceableCollection`.
+    public func nonemptyFlatMap<NewValue>(
+        _ transform: @escaping (Value) -> EventLoopFuture<NewValue>
+    ) -> EventLoopFuture<NewValue> where Value: Collection, NewValue: RangeReplaceableCollection {
+        return self.nonemptyFlatMap(or: .init(), transform)
+    }
+
 }
--- Tests/AsyncKitTests/FutureExtensionsTests.swift
+++ Tests/AsyncKitTests/FutureExtensionsTests.swift
@@ -22,6 +22,33 @@ final class FutureExtensionsTests: XCTestCase {
         try XCTAssertThrowsError(future2.wait())
     }
     
+    func testNonempty() {
+        try XCTAssertNoThrow(self.eventLoop.future([0]).nonempty(orError: TestError.notEqualTo1).wait())
+        try XCTAssertThrowsError(self.eventLoop.future([]).nonempty(orError: TestError.notEqualTo1).wait())
+        
+        XCTAssertEqual(try self.eventLoop.future([0]).nonemptyMap(or: 1, { $0[0] }).wait(), 0)
+        XCTAssertEqual(try self.eventLoop.future([]).nonemptyMap(or: 1, { $0[0] }).wait(), 1)
+
+        XCTAssertEqual(try self.eventLoop.future([0]).nonemptyMap({ [$0[0]] }).wait(), [0])
+        XCTAssertEqual(try self.eventLoop.future([Int]()).nonemptyMap({ [$0[0]] }).wait(), [])
+
+        XCTAssertEqual(try self.eventLoop.future([0]).nonemptyFlatMapThrowing(or: 1, { (a) throws -> Int in a[0] }).wait(), 0)
+        XCTAssertEqual(try self.eventLoop.future([]).nonemptyFlatMapThrowing(or: 1, { (a) throws -> Int in a[0] }).wait(), 1)
+
+        XCTAssertEqual(try self.eventLoop.future([0]).nonemptyFlatMapThrowing({ (a) throws -> [Int] in [a[0]] }).wait(), [0])
+        XCTAssertEqual(try self.eventLoop.future([Int]()).nonemptyFlatMapThrowing({ (a) throws -> [Int] in [a[0]] }).wait(), [])
+
+        XCTAssertEqual(try self.eventLoop.future([0]).nonemptyFlatMap(or: 1, { self.eventLoop.future($0[0]) }).wait(), 0)
+        XCTAssertEqual(try self.eventLoop.future([]).nonemptyFlatMap(or: 1, { self.eventLoop.future($0[0]) }).wait(), 1)
+
+        XCTAssertEqual(try self.eventLoop.future([0]).nonemptyFlatMap(orFlat: self.eventLoop.future(1), { self.eventLoop.future($0[0]) }).wait(), 0)
+        XCTAssertEqual(try self.eventLoop.future([]).nonemptyFlatMap(orFlat: self.eventLoop.future(1), { self.eventLoop.future($0[0]) }).wait(), 1)
+
+        XCTAssertEqual(try self.eventLoop.future([0]).nonemptyFlatMap({ self.eventLoop.future([$0[0]]) }).wait(), [0])
+        XCTAssertEqual(try self.eventLoop.future([Int]()).nonemptyFlatMap({ self.eventLoop.future([$0[0]]) }).wait(), [])
+
+    }
+    
     /// This TestCases EventLoopGroup
     var group: EventLoopGroup!
     
