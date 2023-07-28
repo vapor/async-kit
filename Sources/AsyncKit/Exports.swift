4ee8c9750092f33f3cc942e2c00400416c765565
Gwynne Raskind <gwynne@vapor.codes>
2023-07-27 23:21:27 -0500

    Add CODEOWNERS, fixup Exports.swift

--- Sources/AsyncKit/Exports.swift
+++ Sources/AsyncKit/Exports.swift
@@ -1,4 +1,13 @@
-#if !BUILDING_DOCC
+#if swift(>=5.8)
+
+@_documentation(visibility: internal) @_exported import class NIO.EmbeddedEventLoop
+@_documentation(visibility: internal) @_exported import protocol NIO.EventLoop
+@_documentation(visibility: internal) @_exported import protocol NIO.EventLoopGroup
+@_documentation(visibility: internal) @_exported import class NIO.EventLoopFuture
+@_documentation(visibility: internal) @_exported import struct NIO.EventLoopPromise
+@_documentation(visibility: internal) @_exported import class NIO.MultiThreadedEventLoopGroup
+
+#else
 
 @_exported import class NIO.EmbeddedEventLoop
 @_exported import protocol NIO.EventLoop
