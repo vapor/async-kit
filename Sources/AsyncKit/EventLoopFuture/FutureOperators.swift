bd3064588db6c6637761d36268f88be5569ae87a
Gwynne Raskind <gwynne@vapor.codes>
2023-10-31 07:22:26 -0500

    Fix the future operators (just 'cause)

--- Sources/AsyncKit/EventLoopFuture/FutureOperators.swift
+++ Sources/AsyncKit/EventLoopFuture/FutureOperators.swift
@@ -1,283 +1,110 @@
 import NIOCore
 
-// MARK: - +
+// MARK: - Numeric
 
-/// Adds two futures and produces their sum
-public func +<T>(_ lhs: EventLoopFuture<T>, _ rhs: EventLoopFuture<T>) -> EventLoopFuture<T> where T: Numeric {
-    return lhs.and(rhs).map { (arg) -> (T) in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal + rhsVal
+extension EventLoopFuture where Value: Numeric {
+    /// Adds two futures and produces their sum
+    public static func + (lhs: EventLoopFuture, rhs: EventLoopFuture) -> EventLoopFuture { lhs.and(rhs).map { $0 + $1 } }
+
+    /// Adds two futures and stores the result in the left-hand-side variable
+    public static func += (lhs: inout EventLoopFuture, rhs: EventLoopFuture) { lhs = lhs + rhs }
+
+    /// Subtracts one future from another and produces their difference
+    public static func - (_ lhs: EventLoopFuture, _ rhs: EventLoopFuture) -> EventLoopFuture { lhs.and(rhs).map { $0 - $1 } }
+
+    /// Subtracts the second future from the first and stores the difference in the left-hand-side variable
+    public static func -= (_ lhs: inout EventLoopFuture, _ rhs: EventLoopFuture) { lhs = lhs - rhs }
+
+    /// Multiplies two futures and produces their product
+    public static func * (lhs: EventLoopFuture, rhs: EventLoopFuture) -> EventLoopFuture { lhs.and(rhs).map { $0 * $1 } }
+
+    /// Multiplies two futures and stores the result in the left-hand-side variable
+    public static func *= (lhs: inout EventLoopFuture, rhs: EventLoopFuture) { lhs = lhs * rhs }
+}
+
+// MARK: - Array<Equatable>
+
+extension EventLoopFuture {
+    /// Adds two futures and produces their sum
+    public static func + <T>(lhs: EventLoopFuture, rhs: EventLoopFuture) -> EventLoopFuture where Value == Array<T>, T: Equatable { lhs.and(rhs).map { $0 + $1 } }
+
+    /// Adds two futures and stores the result in the left-hand-side variable
+    public static func += <T>(lhs: inout EventLoopFuture, rhs: EventLoopFuture) where Value == Array<T>, T: Equatable { lhs = lhs + rhs }
+
+    /// Subtracts one future from another and produces their difference
+    public static func - <T>(lhs: EventLoopFuture, rhs: EventLoopFuture) -> EventLoopFuture where Value == Array<T>, T: Equatable {
+        lhs.and(rhs).map { l, r in l.filter { !r.contains($0) } }
     }
+
+    /// Subtracts the second future from the first and stores the difference in the left-hand-side variable
+    public static func -= <T>(lhs: inout EventLoopFuture, rhs: EventLoopFuture) where Value == Array<T>, T: Equatable { lhs = lhs - rhs }
 }
 
-/// Adds two futures and stores the result in the left-hand-side variable
-public func +=<T>(_ lhs: inout EventLoopFuture<T>, _ rhs: EventLoopFuture<T>) where T: Numeric {
-    lhs = lhs.and(rhs).map({ (arg) -> (T) in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal + rhsVal
-    })
+// MARK: - BinaryInteger (division)
+
+extension EventLoopFuture where Value: BinaryInteger {
+    /// Returns the quotient of dividing the first future by the second
+    public static func / (lhs: EventLoopFuture, rhs: EventLoopFuture) -> EventLoopFuture { lhs.and(rhs).map { $0 / $1 } }
+
+    /// Divides the first future by the second and stores the quotient in the left-hand-side variable
+    public static func /= (lhs: inout EventLoopFuture, rhs: EventLoopFuture) { lhs = lhs / rhs }
+
+    /// Returns the remainder of dividing the first future by the second
+    public static func % (lhs: EventLoopFuture, rhs: EventLoopFuture) -> EventLoopFuture { lhs.and(rhs).map { $0 % $1 } }
+
+    /// Divides the first future by the second and stores the remainder in the left-hand-side variable
+    public static func %= (lhs: inout EventLoopFuture, rhs: EventLoopFuture) { lhs = lhs % rhs }
 }
 
-/// Adds two futures and produces their sum
-public func +<T>(_ lhs: EventLoopFuture<[T]>, _ rhs: EventLoopFuture<[T]>) -> EventLoopFuture<[T]> {
-    return lhs.and(rhs).map { (arg) -> ([T]) in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal + rhsVal
-    }
+// MARK: - BinaryInteger (comparison)
+
+extension EventLoopFuture where Value: BinaryInteger {
+    /// Returns a Boolean value indicating whether the value of the first argument is less than that of the second argument
+    public static func < (lhs: EventLoopFuture, rhs: EventLoopFuture<some BinaryInteger>) -> EventLoopFuture<Bool> { lhs.and(rhs).map { $0 < $1 } }
+
+    /// Returns a Boolean value indicating whether the value of the first argument is less than or equal to that of the second argument
+    public static func <= (lhs: EventLoopFuture, rhs: EventLoopFuture<some BinaryInteger>) -> EventLoopFuture<Bool> { lhs.and(rhs).map { $0 <= $1 } }
+
+    /// Returns a Boolean value indicating whether the value of the first argument is greater than or equal to that of the second argument
+    public static func >= (lhs: EventLoopFuture, rhs: EventLoopFuture<some BinaryInteger>) -> EventLoopFuture<Bool> { lhs.and(rhs).map { $0 >= $1 } }
+
+    /// Returns a Boolean value indicating whether the value of the first argument is greater than that of the second argument
+    public static func > (lhs: EventLoopFuture, rhs: EventLoopFuture<some BinaryInteger>) -> EventLoopFuture<Bool> { lhs.and(rhs).map { $0 > $1 } }
 }
 
-/// Adds two futures and stores the result in the left-hand-side variable
-public func +=<T>(_ lhs: inout EventLoopFuture<[T]>, _ rhs: EventLoopFuture<[T]>) {
-    lhs = lhs.and(rhs).map({ (arg) -> ([T]) in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal + rhsVal
-    })
-}
+// MARK: - BinaryInteger (bitwise)
 
+extension EventLoopFuture where Value: BinaryInteger {
+    /// Returns the result of shifting a future’s binary representation the specified number of digits to the left
+    public static func << (lhs: EventLoopFuture, rhs: EventLoopFuture<some BinaryInteger>) -> EventLoopFuture { lhs.and(rhs).map { $0 << $1 } }
 
-// MARK: - -
+    /// Stores the result of shifting a future’s binary representation the specified number of digits to the left in the left-hand-side variable
+    public static func <<= (lhs: inout EventLoopFuture, rhs: EventLoopFuture<some BinaryInteger>) { lhs = lhs << rhs }
 
-/// Subtracts one future from another and produces their difference
-public func -<T>(_ lhs: EventLoopFuture<T>, _ rhs: EventLoopFuture<T>) -> EventLoopFuture<T> where T: Numeric {
-    return lhs.and(rhs).map { (arg) -> (T) in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal - rhsVal
-    }
-}
+    /// Returns the result of shifting a future’s binary representation the specified number of digits to the right
+    public static func >> (lhs: EventLoopFuture, rhs: EventLoopFuture<some BinaryInteger>) -> EventLoopFuture { lhs.and(rhs).map { $0 >> $1 } }
 
-/// Subtracts the second future from the first and stores the difference in the left-hand-side variable
-public func -=<T>(_ lhs: inout EventLoopFuture<T>, _ rhs: EventLoopFuture<T>) where T: Numeric {
-    lhs = lhs.and(rhs).map { (arg) -> (T) in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal - rhsVal
-    }
-}
+    /// Stores the result of shifting a future’s binary representation the specified number of digits to the right in the left-hand-side variable
+    public static func >>= (lhs: inout EventLoopFuture, rhs: EventLoopFuture<some BinaryInteger>) { lhs = lhs >> rhs }
 
-/// Subtracts one future from another and produces their difference
-public func -<T>(_ lhs: EventLoopFuture<[T]>, _ rhs: EventLoopFuture<[T]>) -> EventLoopFuture<[T]> where T: Equatable {
-    return lhs.and(rhs).map { (arg) -> ([T]) in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal.filter({ val -> Bool in
-            return !rhsVal.contains(val)
-        })
-    }
-}
+    /// Returns the result of performing a bitwise AND operation on the two given futures
+    public static func & (lhs: EventLoopFuture, rhs: EventLoopFuture) -> EventLoopFuture { lhs.and(rhs).map { $0 & $1 } }
 
-/// Subtracts the second future from the first and stores the difference in the left-hand-side variable
-public func -=<T>(_ lhs: inout EventLoopFuture<[T]>, _ rhs: EventLoopFuture<[T]>) where T: Equatable {
-    lhs = lhs.and(rhs).map { (arg) -> ([T]) in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal.filter({ val -> Bool in
-            return !rhsVal.contains(val)
-        })
-    }
-}
+    /// Stores the result of performing a bitwise AND operation on the two given futures in the left-hand-side variable
+    public static func &= (lhs: inout EventLoopFuture, rhs: EventLoopFuture) { lhs = lhs & rhs }
 
-// MARK: - *
+    /// Returns the result of performing a bitwise OR operation on the two given futures
+    public static func | (lhs: EventLoopFuture, rhs: EventLoopFuture) -> EventLoopFuture { lhs.and(rhs).map { $0 | $1 } }
 
-/// Multiplies two futures and produces their product
-public func *<T>(_ lhs: EventLoopFuture<T>, _ rhs: EventLoopFuture<T>) -> EventLoopFuture<T> where T: Numeric {
-    return lhs.and(rhs).map { (arg) -> (T) in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal * rhsVal
-    }
-}
+    /// Stores the result of performing a bitwise OR operation on the two given futures in the left-hand-side variable
+    public static func |= (lhs: inout EventLoopFuture, rhs: EventLoopFuture) { lhs = lhs | rhs }
 
-/// Multiplies two futures and stores the result in the left-hand-side variable
-public func *=<T>(_ lhs: inout EventLoopFuture<T>, _ rhs: EventLoopFuture<T>) where T: Numeric {
-    lhs = lhs.and(rhs).map { (arg) -> (T) in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal * rhsVal
-    }
-}
+    /// Returns the result of performing a bitwise XOR operation on the two given futures
+    public static func ^ (lhs: EventLoopFuture, rhs: EventLoopFuture) -> EventLoopFuture { lhs.and(rhs).map { $0 ^ $1 } }
 
-// MARK: - %
+    /// Stores the result of performing a bitwise XOR operation on the two given futures in the left-hand-side variable
+    public static func ^= (lhs: inout EventLoopFuture, rhs: EventLoopFuture) { lhs = lhs ^ rhs }
 
-/// Returns the remainder of dividing the first future by the second
-public func %<T>(_ lhs: EventLoopFuture<T>, _ rhs: EventLoopFuture<T>) -> EventLoopFuture<T> where T: BinaryInteger {
-    return lhs.and(rhs).map { (arg) -> (T) in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal % rhsVal
-    }
-}
-
-/// Divides the first future by the second and stores the remainder in the left-hand-side variable
-public func %=<T>(_ lhs: inout EventLoopFuture<T>, _ rhs: EventLoopFuture<T>) where T: BinaryInteger {
-    lhs = lhs.and(rhs).map { (arg) -> (T) in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal % rhsVal
-    }
-}
-
-// MARK: - &
-
-/// Returns the result of performing a bitwise AND operation on the two given futures
-public func &<T>(_ lhs: EventLoopFuture<T>, _ rhs: EventLoopFuture<T>) -> EventLoopFuture<T> where T: BinaryInteger {
-    return lhs.and(rhs).map { (arg) -> (T) in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal & rhsVal
-    }
-}
-
-/// Stores the result of performing a bitwise AND operation on the two given futures in the left-hand-side variable
-public func &=<T>(_ lhs: inout EventLoopFuture<T>, _ rhs: EventLoopFuture<T>) where T: BinaryInteger {
-    lhs = lhs.and(rhs).map { (arg) -> (T) in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal & rhsVal
-    }
-}
-
-// MARK: - /
-
-/// Returns the quotient of dividing the first future by the second
-public func /<T>(_ lhs: EventLoopFuture<T>, _ rhs: EventLoopFuture<T>) -> EventLoopFuture<T> where T: BinaryInteger {
-    return lhs.and(rhs).map { (arg) -> (T) in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal / rhsVal
-    }
-}
-
-/// Divides the first future by the second and stores the quotient in the left-hand-side variable
-public func /=<T>(_ lhs: inout EventLoopFuture<T>, _ rhs: EventLoopFuture<T>) where T: BinaryInteger {
-    lhs = lhs.and(rhs).map { (arg) -> (T) in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal / rhsVal
-    }
-}
-
-// MARK: - Comparison
-
-/// Returns a Boolean value indicating whether the value of the first argument is less than that of the second argument
-public func < <T, Other>(_ lhs: EventLoopFuture<T>, _ rhs: EventLoopFuture<Other>) -> EventLoopFuture<Bool> where T: BinaryInteger, Other: BinaryInteger {
-    return lhs.and(rhs).map { (arg) -> Bool in
-        let (lhsVal, rhsVal) = (arg)
-        return lhsVal < rhsVal
-    }
-}
-
-/// Returns a Boolean value indicating whether the value of the first argument is less than or equal to that of the second argument
-public func <=<T>(_ lhs: EventLoopFuture<T>, _ rhs: EventLoopFuture<T>) -> EventLoopFuture<Bool> where T: BinaryInteger {
-    return lhs.and(rhs).map { (arg) -> Bool in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal <= rhsVal
-    }
-}
-
-/// Returns a Boolean value indicating whether the value of the first argument is less than or equal to that of the second argument
-public func <=<T, Other>(_ lhs: EventLoopFuture<T>, _ rhs: EventLoopFuture<Other>) -> EventLoopFuture<Bool> where T: BinaryInteger, Other: BinaryInteger {
-    return lhs.and(rhs).map { (arg) -> Bool in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal <= rhsVal
-    }
-}
-
-/// Returns a Boolean value indicating whether the value of the first argument is greater than or equal to that of the second argument
-public func >=<T>(_ lhs: EventLoopFuture<T>, _ rhs: EventLoopFuture<T>) -> EventLoopFuture<Bool> where T: BinaryInteger {
-    return lhs.and(rhs).map { (arg) -> Bool in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal >= rhsVal
-    }
-}
-
-/// Returns a Boolean value indicating whether the value of the first argument is greater than or equal to that of the second argument
-public func >=<T, Other>(_ lhs: EventLoopFuture<T>, _ rhs: EventLoopFuture<Other>) -> EventLoopFuture<Bool> where T: BinaryInteger, Other: BinaryInteger {
-    return lhs.and(rhs).map { (arg) -> Bool in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal >= rhsVal
-    }
-}
-
-/// Returns a Boolean value indicating whether the value of the first argument is greater than that of the second argument
-public func ><T>(_ lhs: EventLoopFuture<T>, _ rhs: EventLoopFuture<T>) -> EventLoopFuture<Bool> where T: BinaryInteger {
-    return lhs.and(rhs).map { (arg) -> Bool in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal > rhsVal
-    }
-}
-
-/// Returns a Boolean value indicating whether the value of the first argument is greater than that of the second argument
-public func ><T, Other>(_ lhs: EventLoopFuture<T>, _ rhs: EventLoopFuture<Other>) -> EventLoopFuture<Bool> where T: BinaryInteger, Other: BinaryInteger {
-    return lhs.and(rhs).map { (arg) -> Bool in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal > rhsVal
-    }
-}
-
-// MARK: - Bitshifts
-
-/// Returns the result of shifting a future’s binary representation the specified number of digits to the left
-public func << <T, RHS>(_ lhs: EventLoopFuture<T>, _ rhs: EventLoopFuture<RHS>) -> EventLoopFuture<T> where T: BinaryInteger, RHS: BinaryInteger {
-    return lhs.and(rhs).map { (arg) -> T in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal << rhsVal
-    }
-}
-
-/// Stores the result of shifting a future’s binary representation the specified number of digits to the left in the left-hand-side variable
-public func <<= <T, RHS>(_ lhs: inout EventLoopFuture<T>, _ rhs: EventLoopFuture<RHS>) where T: BinaryInteger, RHS: BinaryInteger {
-    lhs = lhs.and(rhs).map { (arg) -> T in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal << rhsVal
-    }
-}
-
-/// Returns the result of shifting a future’s binary representation the specified number of digits to the right
-public func >> <T, RHS>(_ lhs: EventLoopFuture<T>, _ rhs: EventLoopFuture<RHS>) -> EventLoopFuture<T> where T: BinaryInteger, RHS: BinaryInteger {
-    return lhs.and(rhs).map { (arg) -> T in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal >> rhsVal
-    }
-}
-
-/// Stores the result of shifting a future’s binary representation the specified number of digits to the right in the left-hand-side variable
-public func >>= <T, RHS>(_ lhs: inout EventLoopFuture<T>, _ rhs: EventLoopFuture<RHS>) where T: BinaryInteger, RHS: BinaryInteger {
-    lhs = lhs.and(rhs).map { (arg) -> T in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal >> rhsVal
-    }
-}
-
-// MARK: - ^
-
-/// Returns the result of performing a bitwise XOR operation on the two given futures
-public func ^<T>(_ lhs: EventLoopFuture<T>, _ rhs: EventLoopFuture<T>) -> EventLoopFuture<T> where T: BinaryInteger {
-    return lhs.and(rhs).map { (arg) -> (T) in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal ^ rhsVal
-    }
-}
-
-/// Stores the result of performing a bitwise XOR operation on the two given futures in the left-hand-side variable
-public func ^=<T>(_ lhs: inout EventLoopFuture<T>, _ rhs: EventLoopFuture<T>) where T: BinaryInteger {
-    lhs = lhs.and(rhs).map { (arg) -> (T) in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal ^ rhsVal
-    }
-}
-
-// MARK: - |
-
-/// Returns the result of performing a bitwise OR operation on the two given futures
-public func |<T>(_ lhs: EventLoopFuture<T>, _ rhs: EventLoopFuture<T>) -> EventLoopFuture<T> where T: BinaryInteger {
-    return lhs.and(rhs).map { (arg) -> (T) in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal | rhsVal
-    }
-}
-
-/// Stores the result of performing a bitwise OR operation on the two given futures in the left-hand-side variable
-public func |=<T>(_ lhs: inout EventLoopFuture<T>, _ rhs: EventLoopFuture<T>) where T: BinaryInteger {
-    lhs = lhs.and(rhs).map { (arg) -> (T) in
-        let (lhsVal, rhsVal) = arg
-        return lhsVal | rhsVal
-    }
-}
-
-// MARK: - ~
-
-/// Returns the result of performing a bitwise NOT operation on the given future
-prefix func ~<T>(_ x: EventLoopFuture<T>) -> EventLoopFuture<T> where T: BinaryInteger {
-    return x.map { xVal -> T in
-        return ~xVal
-    }
+    /// Returns the result of performing a bitwise NOT operation on the given future
+    public static prefix func ~ (x: EventLoopFuture) -> EventLoopFuture { x.map { ~$0 } }
 }
